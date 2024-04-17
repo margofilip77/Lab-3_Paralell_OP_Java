5fa7a33d467a2f3ebb43977b807235e14eca8280
margofilip77 <95464589+margofilip77@users.noreply.github.com>
2024-04-17 15:47:26 +0300

    Lab_3/Java

--- src/Main.java
+++ src/Main.java
@@ -7,13 +7,11 @@ public class Main {
 
         Manage manage = new Manage(manageCapacity);
 
-        // Створюємо та запускаємо потоки продюсерів
         for (int i = 0; i < totalProducers; i++) {
             Thread producerThread = new Thread(new Producer(manage, totalProducts));
             producerThread.start();
         }
-
-        // Створюємо та запускаємо потоки консюмерів
+        
         for (int i = 0; i < totalConsumers; i++) {
             Thread consumerThread = new Thread(new Consumer(manage, totalProducts));
             consumerThread.start();
